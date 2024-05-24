08edd74b8b0b68c987d20176d2cc3c665a79aa47
Kirshan Thakur <166369674+Thakurkirshana@users.noreply.github.com>
2024-05-24 16:51:45 +0500

    atlib

---
+++ class 3/app.js
@@ -0,0 +1,76 @@
+
+// let pro = new Promise((res, rej) =>{
+//     let khilao = 'Zinger khilaao'
+//     if (khilao == 'khilaao'){
+//         res ('han bhai theak hai')
+//     }
+//     else{
+//         rej ('nikar bhai ')
+//     }
+// })
+
+// pro. then( (data) => {
+//     console.log (data) 
+// })
+
+// .catch((err)=>{
+//     console.log(err)
+// })
+
+
+// let pro = new Promise((res, rej) => {
+//     let pakistan = " pakistan world cup jeeti ga"
+//     if (pakistan == " pakistan world cup jeeti ga"){
+//         res ('cup jeeti ga')
+//     }
+
+//     else{
+//         rej ('nhi jeeti ga pakistan')
+//     }
+// })
+//    pro. then((data)=> {
+//     console.log(data)
+//    })
+//  .catch((err)=>{
+//     console.log(err)
+//  })
+
+
+
+// DESTRUCTURING ( array, object)
+// let arr = ['ali','rehman ',1,2,3,4]
+// let [a,b,c,d,e,f] =arr 
+// console.log(c) 
+
+
+// const obj = {
+//     abc :'ABCD',
+//     def : '24',
+//     ghi : 'Hello World'
+
+// }
+
+
+const obj = {
+    abc : 'ABCD',
+    cde : '24',
+    efg : ' Hello World'  
+    
+    arr : [ 1, 2, 3]
+const {
+    abc, cde 
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+

46acb70623feab997297f6f19cd55506a9ac7b8e
Kirshan Thakur <166369674+Thakurkirshana@users.noreply.github.com>
2024-05-25 20:55:24 +0500

    ecmascript1

--- class 3/app.js
+++ class 3/app.js
@@ -51,26 +51,69 @@
 // }
 
 
-const obj = {
-    abc : 'ABCD',
-    cde : '24',
-    efg : ' Hello World'  
-    
-    arr : [ 1, 2, 3]
-const {
-    abc, cde 
+// const obj = {
+//     abc : 'ABCD',
+//     def : '24',
+//     ghi : ' Hello World',  
+//     arr : [1,2,3]
+// }
+// const{ abc, def, ghi, arr } = obj 
+// const [a, b, c] = arr
+// document.write(a)
+
+
+// fetch('https://jsonplaceholder.typicode.com/todos' )
+// .then(data => data.json())
+// .then(data=> console.log(data[177].title))
+// .catch(err=> console.log(err))
+
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//  .then(data => data.json())
+//  .then(data => document.write(data))
+//  .catch((err) => {
+//     console.log(err)
+//  })
+
+let obj = [
+{
+p_id : '01',
+p_des : 'Hello World',
+p_img : 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGhvdG98ZW58MHx8MHx8fDA%3D'
+},
+{
+    p_id : '02',
+    p_des : 'Hello World',
+    p_img : 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGhvdG98ZW58MHx8MHx8fDA%3D'
+    },
+
+    {
+        p_id : '03',
+        p_des : 'Hello World',
+        p_img : 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGhvdG98ZW58MHx8MHx8fDA%3D'
+        },
+]
+  var products = document.getElementById('show')
+for (var i= 0; i < obj.length; i++){
+    for(var key in obj ){
+        // console.log(obj[key])
+       for ( var i =0; i< obj.length; i++){
+        var pro = obj[i]
+        var p = document.createElement('p')
+        p.innerHTML = pro.p_id
+        products.appendChild(p)
+         var pdes = document.createElement('p')
+         products.appendChild(pdes)
+   var pimg = document.createElement('img')
+   pimg.src = pro.p_img
+   products.appendChild(pimg)
+   pimg.setAttribute('width','100')
+
+
+
+
+       }
+
+    }
 }
-
-}
-
-
-
-
-
-
-
-
-
-
 
 
